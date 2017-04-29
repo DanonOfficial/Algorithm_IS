a5303e9f910b2090b53628451e8e7ca8530d92b2
Daniil <DanonOfficial@users.noreply.github.com>
2017-04-29 22:27:22 +0300

    Update Task#1.cpp

--- Lab-6/Task#1.cpp
+++ Lab-6/Task#1.cpp
@@ -12,18 +12,15 @@ int main(){
 
     graph.resize(n, vector<int>(n,0));
     int from, to;
-    //cout << graph.size() <<" kekus " << m << endl;
     for(int i = 0; i < m; i++){
         cin >> from >> to;
         graph[from-1][to-1] = 1;
     }
-    //cout << "here" << endl;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             cout << graph[i][j] << " ";
         }
         cout << endl;
     }
-    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
